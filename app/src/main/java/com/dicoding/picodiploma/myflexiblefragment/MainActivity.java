c2d36e26f3279e5183668cb4fa2daf03afb318c6
ndgspn <nandangsopyan08@gmail.com>
2019-05-08 10:47:22 +0700

    added HomeFragment.java and linked to MainActivity

--- app/src/main/java/com/dicoding/picodiploma/myflexiblefragment/MainActivity.java
+++ app/src/main/java/com/dicoding/picodiploma/myflexiblefragment/MainActivity.java
@@ -1,7 +1,11 @@
 package com.dicoding.picodiploma.myflexiblefragment;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager mFramentManager = getSupportFragmentManager();
+        FragmentTransaction mFragmentTransaction = mFramentManager.beginTransaction();
+        HomeFragment mHomeFragment = new HomeFragment();
+
+        Fragment fragment = mFramentManager.findFragmentByTag(HomeFragment.class.getSimpleName());
+        if (!(fragment instanceof HomeFragment)) {
+            mFragmentTransaction.add(R.id.frame_container, mHomeFragment, HomeFragment.class.getSimpleName());
+            Log.d("MyFlexibleFragment", "Fragment Name : " + HomeFragment.class.getSimpleName());
+            mFragmentTransaction.commit();
+        }
 
     }
 }
