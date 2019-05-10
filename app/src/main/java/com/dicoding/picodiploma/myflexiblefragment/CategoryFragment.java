c3e78da433755f3780aa8dc82a6c78c92e8925b8
ndgspn <nandangsopyan08@gmail.com>
2019-05-10 11:21:27 +0700

    Completing HomeFrament code

--- app/src/main/java/com/dicoding/picodiploma/myflexiblefragment/CategoryFragment.java
+++ app/src/main/java/com/dicoding/picodiploma/myflexiblefragment/CategoryFragment.java
@@ -2,16 +2,19 @@ package com.dicoding.picodiploma.myflexiblefragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CategoryFragment extends Fragment {
+public class CategoryFragment extends Fragment implements View.OnClickListener {
 
 
     public CategoryFragment() {
@@ -26,4 +29,17 @@ public class CategoryFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_category, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btnDetailCategory = view.findViewById(R.id.btn_detail_category);
+        btnDetailCategory.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_detail_category) {
+        }
+
+    }
 }
