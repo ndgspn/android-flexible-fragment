158cae84286cd5a6b2ebb63fc99f4d4335cccc64
ndgspn <nandangsopyan08@gmail.com>
2019-05-12 14:06:21 +0700

    Mengirim data antar fragment, fragment untuk dialog, memanggil activity dari fragment

--- app/src/main/java/com/dicoding/picodiploma/myflexiblefragment/DetailCategoryFragment.java
+++ app/src/main/java/com/dicoding/picodiploma/myflexiblefragment/DetailCategoryFragment.java
@@ -1,15 +1,18 @@
 package com.dicoding.picodiploma.myflexiblefragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -69,9 +72,20 @@ public class DetailCategoryFragment extends Fragment implements View.OnClickList
     public  void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_profile:
+                Intent mIntent = new Intent(getActivity(), ProfileActivity.class);
+                startActivity(mIntent);
                 break;
             case R.id.btn_show_dialog:
+                OptionDialogFragment mOptionDialogFragment = new OptionDialogFragment();
+                FragmentManager mFragmentManager = getChildFragmentManager();
+                mOptionDialogFragment.show(mFragmentManager, OptionDialogFragment.class.getSimpleName());
                 break;
         }
     }
+    OptionDialogFragment.OnOptionDialogListener optionDialogListener = new OptionDialogFragment.OnOptionDialogListener() {
+        @Override
+        public void onOptionChosen(String text) {
+            Toast.makeText(getActivity(), text, Toast.LENGTH_SHORT).show();
+        }
+    };
 }
